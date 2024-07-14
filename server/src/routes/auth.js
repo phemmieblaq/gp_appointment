3e51dddab7ca118c0acf81470afbd744eb19eb8f
bamidele2161 <akinyemibamidele2@gmail.com>
2024-07-14 07:57:17 +0100

    restructuring

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,9 +1,10 @@
 import { GetUsersByRole, Login, UserRegistration } from "../controller/auth";
+import authorizer from "../middleware/auth";
 const express = require("express");
 const router = express.Router();
 
 router.post("/", UserRegistration);
 router.post("/login", Login);
-router.get("/role", GetUsersByRole);
+router.get("/role", authorizer("PATIENT"), GetUsersByRole);
 
 module.exports = router;
